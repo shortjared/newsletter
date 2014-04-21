6cc674070e7d67f5ac09e10f8b195924efdfd9be
Jared Short <jaredlshort@gmail.com>
2014-04-21 17:49:00 -0400

    File Cleanup

--- handlers/failure.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$message = '<p>Uh oh! Something has gone wrong, you weren\'t subscrbed. <br /> <a href="/contact">Let us know!</a></p>';
-
-$page->title = i18n_get ('Something Went Wrong!');
-echo $tpl->render('newsletter/complete',array('message'=>$message));
-
-?>
